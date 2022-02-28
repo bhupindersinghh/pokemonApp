6477d5293ed86ff607c1ba89e16b493c24bf3674
bhupindersinghh <69050388+bhupindersinghh@users.noreply.github.com>
2022-02-28 12:28:21 +0530

    Update App.js
    Removing uneccesary code

--- POKE/src/App.js
+++ POKE/src/App.js
@@ -121,15 +121,9 @@ function App() {
                   stat1={pokemon.stats[0].stat.name}
                   stat2={pokemon.stats[1].stat.name}
                   stat3={pokemon.stats[2].stat.name}
-                  stat4={pokemon.stats[3].stat.name}
-                  stat5={pokemon.stats[4].stat.name}
-                  stat6={pokemon.stats[5].stat.name}
                   bs1={pokemon.stats[0].base_stat}
                   bs2={pokemon.stats[1].base_stat}
                   bs3={pokemon.stats[2].base_stat}
-                  bs4={pokemon.stats[3].base_stat}
-                  bs5={pokemon.stats[4].base_stat}
-                  bs6={pokemon.stats[5].base_stat}
                 />
               ))}
             </div>
